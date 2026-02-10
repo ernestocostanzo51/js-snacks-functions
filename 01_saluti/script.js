aca893f9bce57b7e9be8fecec0efe8e4bc16942e
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-10 12:55:42 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,12 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+// Dichiara la funzione qui.
+const saluto = nome =>console.log("ciao", nome) 
+
+// Invoca la funzione qui e stampa il risultato in console
+
+saluto(userName)
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
