4a408ba76140acbcae2e52e610471a9f607cc200
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-10 16:25:11 +0100

    commit secodno snack

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function iniziali (nomi){
+    let iniziale=[]
+    for(i=0; i<nomi.length; i++){
+    if (nomi[i].length > 0) {
+            iniziale.push(nomi[i][0]);
+        }
+
+}
+return iniziale
+}
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let soluzione = iniziali(names)
+console.log(soluzione)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function iniziali (nomi){
+    let iniziale=[]
+    for(i=0; i<nomi.length; i++){
+    if (nomi[i].length === "A") {
+            iniziale.push(nomi);
+        }
+
+}
+return iniziale
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let soluzione = iniziali(names)
+console.log(soluzione)
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
